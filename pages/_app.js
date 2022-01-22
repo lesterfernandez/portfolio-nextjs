c794bb3747ae12cf27101b14775563bf6c24cdc8
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-01-22 13:15:38 -0500

    feat: added meta tags and title

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
 import Nav from "../components/Nav";
 import Section from "../components/Section";
 import theme from "../components/theme";
@@ -7,6 +8,14 @@ import theme from "../components/theme";
 function MyApp({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Lester Fernandez</title>
+        <meta
+          name="description"
+          content="Portfolio website for Lester Fernandez, a software development."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Nav />
       <AnimatePresence exitBeforeEnter initial>
         <Section key={router.asPath}>
